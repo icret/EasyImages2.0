d81d31d55ead6ec68484ea2399acc640fe47c791
icret <lemonim@qq.com>
2019-06-29 08:55:24 +0800

    添加注释

--- config.php
+++ config.php
@@ -19,6 +19,8 @@ ini_set('memory_limit', '512M');
 // 定义当前目录
 define('APP_ROOT', __DIR__);
 
+/******** 网站配置 ********/
+
 $config = array(
     // 网站标题
      'title' => '简单图床 - EasyImage',
@@ -28,11 +30,11 @@ $config = array(
      'description' => '简单图床EasyImage是一款支持多文件上传的无数据库图床,可以完美替代PHP多图上传程序，最新html5自适应页面兼容手机电脑，上传后返回图片直链，markdown图片，论坛贴图bbscode链接，简单方便支持一键复制，支持多域名，api上传。',
     // 网站公告
      'tips' => ' 单个文件限制5M，每次最多上传30张图片,本网站仅做演示用，不对图片负任何责任。',
-    // 当前域名,末尾不加"/" 如果是异域上传请修改为当前异域域名
+    // 图片直链域名 末尾不加"/" 如果你想上传域名是a.com但是我想上传之后返回域名是b.com  那就在这里填写b.com
      'domain' => 'https://img.545141.com',
     // 存储路径 前后要加"/" 如更改此目录，需要同步修改tinyfilemanager.php中的$directories_users路径
      'path' => '/i/',
-    // 最大上传限制 默认为5m 请使用工具转换mb http://www.bejson.com/convert/filesize/
+    // 最大上传限制 默认为 5242880Bytes = 5MB 请使用工具转换MB http://www.bejson.com/convert/filesize/
      'maxSize' => 5242880,
     // 是否开启登录上传 开启:true 关闭false
      'mustLogin' => false,
@@ -53,11 +55,11 @@ $config = array(
      * 5：图片中心 6：右边居中  7：底部居左 8：底部居中 9：底部居右
      */
     'waterPosition' => 8,
-    // 水印文字颜色 rgba 末尾为透明度0-127 0为不透明
+    // 水印文字颜色 rgba 末尾为透明度 范围:0-127 0为不透明
      'textColor' => '47,79,79,0',
     // 水印文字大小
      'textSize' => 16,
-    // 字体路径 如果想改变字体，请选择支持中文的 gb2312
+    // 字体路径 如果想改变字体 请选择支持中文的gb2312字体 否则中文水印会乱码 纯英文水印字体随便
      'textFont' => APP_ROOT . '/public/static/hkxzy.ttf',
     // 图片水印路径 支持GIF,JPG,BMP,PNG和PNG alpha
      'waterImg' => 'public/static/watermark.png',
@@ -79,11 +81,11 @@ $config = array(
      'image_x' => 1024,
     // 缩减的最大宽度
      'image_y' => 1024,
-    // 开启静态文件CDN 开启true 关闭false
+    // 开启静态文件js css CDN 开启true 关闭false
      'static_cdn' => false,
-    // 开启顶部广告 如果想添加或修改广告请到 public/static/ad_top.html
+    // 开启顶部广告 如果想添加或修改广告修改 public/static/ad_top.html
      'ad_top' => false,
-    //  开启底部广告 如果想添加或修改广告请到 public/static/ad_bot.html
+    //  开启底部广告 如果想添加或修改广告修改 public/static/ad_bot.html
      'ad_bot' => false,
     'Version' => '2.0.2.0',
 );
