3f19610fe034e488d78b641643327e787cc18722
icret <ispro@foxmail.com>
2021-11-09 12:08:23 +0800

    delete /

--- install/index.php
+++ install/index.php
@@ -48,8 +48,8 @@ function checkPASS($name)
     <meta name="keywords" content="EasyIamge 2.0 安装环境检测" />
     <meta name="description" content="EasyIamge 2.0 安装环境检测" />
     <link rel="shortcut icon" href="./../public/favicon.ico" type="image/x-icon" />
-    <link href="./..//public/static/zui/css/zui.min.css?v1.9.2" rel="stylesheet">
-    <script src="./..//public/static/zui/lib/jquery/jquery-3.4.1.min.js?v3.4.1"></script>
+    <link href="./../public/static/zui/css/zui.min.css?v1.9.2" rel="stylesheet">
+    <script src="./../public/static/zui/lib/jquery/jquery-3.4.1.min.js?v3.4.1"></script>
     <script src="./../public/static/zui/js/zui.min.js?v1.9.2"></script>
     <script src="./../public/static/qrcode.min.js?v2.0"></script>
 </head>
